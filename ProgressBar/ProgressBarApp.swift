79fd7afebb5af85496119d70958cdc4b69057cb3
Etishagarg <gargetisha@gmail.com>
2024-07-18 13:04:00 +0530

    Initial Commit

---
+++ ProgressBar/ProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarApp.swift
+//  ProgressBar
+//
+//  Created by Etisha Garg on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
